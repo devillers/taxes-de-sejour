e2e5f35aab2b99e8cde3b1902b2250be7119d0e7
david devillers <davidevillers@gmail.com>
2025-06-18 13:50:42 +0200

    Ajout page d'accueil et menu

---
+++ app/taxe-de-sejour/page.js
@@ -0,0 +1,8 @@
+export default function TaxeDeSejour() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Taxe de s\u00e9jour</h1>
+      <p>Contenu \u00e0 venir...</p>
+    </div>
+  );
+}
